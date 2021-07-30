4b165b1b18ef291dfdb1f238915b7e2c1b6f8749
Dmitry Yunitsky <dmitry.yunitsky@gmail.com>
2021-07-31 00:58:06 +0300

    Add logic to parse 9-patches.

--- app/src/main/java/com/mapbox/maps/testapp/utils/StyleUtils.kt
+++ app/src/main/java/com/mapbox/maps/testapp/utils/StyleUtils.kt
@@ -1,4 +1,41 @@
 package com.mapbox.maps.testapp.utils
 
-class StyleUtils {
+import android.graphics.Bitmap
+import com.mapbox.maps.Image
+import com.mapbox.maps.ImageContent
+import com.mapbox.maps.ImageStretches
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.utils.NinePatchBitmap
+import java.nio.ByteBuffer
+
+fun Bitmap.toImage(): Image {
+  val byteBuffer = ByteBuffer.allocate(byteCount)
+  copyPixelsToBuffer(byteBuffer)
+  return Image(width, height, byteBuffer.array())
+}
+
+fun Style.addNinePatchImage(
+  imageId: String,
+  ninePatchBitmap: NinePatchBitmap,
+) {
+  this.addStyleImage(
+    imageId,
+    scale = pixelRatio,
+    image = ninePatchBitmap.bitmap.toImage(),
+    sdf = false,
+    stretchX = ninePatchBitmap.ninePatchDescription
+      .xCoordinates
+      .map { it.toFloat() }
+      .zipWithNext(::ImageStretches),
+    stretchY = ninePatchBitmap.ninePatchDescription
+      .yCoordinates
+      .map { it.toFloat() }
+      .zipWithNext(::ImageStretches),
+    content = ImageContent(
+      0f + ninePatchBitmap.ninePatchDescription.leftPadding.toFloat(),
+      0f + ninePatchBitmap.ninePatchDescription.topPadding.toFloat(),
+      ninePatchBitmap.bitmap.width.toFloat() - ninePatchBitmap.ninePatchDescription.rightPadding.toFloat(),
+      ninePatchBitmap.bitmap.height.toFloat() - ninePatchBitmap.ninePatchDescription.bottomPadding.toFloat(),
+    ),
+  )
 }
--- extension-style/src/main/java/com/mapbox/maps/extension/style/utils/NinePatchUtils.kt
+++ extension-style/src/main/java/com/mapbox/maps/extension/style/utils/NinePatchUtils.kt
@@ -1,4 +1,73 @@
 package com.mapbox.maps.extension.style.utils
 
-class NinePatchUtils {
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.annotation.DrawableRes
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+data class NinePatchBitmap(
+  val bitmap: Bitmap,
+  val ninePatchDescription: NinePatchDescription,
+)
+
+data class NinePatchDescription(
+  val leftPadding: Int,
+  val rightPadding: Int,
+  val topPadding: Int,
+  val bottomPadding: Int,
+  val xCoordinates: List<Int>,
+  val yCoordinates: List<Int>,
+)
+
+fun Context.parseNinePatchResource(@DrawableRes res: Int): NinePatchBitmap {
+  val bitmap = BitmapFactory.decodeResource(resources, res,)
+
+  val ninePatchBuffer = bitmap.ninePatchChunk
+  checkNotNull(ninePatchBuffer) {
+    "bitmap.ninePatchChunk is null. Is drawable a 9 patch?"
+  }
+
+  return NinePatchBitmap(
+    bitmap,
+    parseNinePatchBuffer(ninePatchBuffer)
+  )
+}
+
+private fun parseNinePatchBuffer(ninePatchBuffer: ByteArray): NinePatchDescription {
+  val buffer = ByteBuffer.wrap(ninePatchBuffer).order(ByteOrder.nativeOrder())
+
+  buffer.get()
+  val xCoordinatesNumber = buffer.get() // second byte - number of x coordinates
+  val yCoordinatesNumber = buffer.get() // third byte - number of y coordinates
+  buffer.get()
+
+  buffer.int
+  buffer.int
+
+  val leftPadding = buffer.int
+  val rightPadding = buffer.int
+  val topPadding = buffer.int
+  val bottomPadding = buffer.int
+
+  buffer.int
+
+  val xCoordinates = mutableListOf<Int>()
+  for (i in (0 until xCoordinatesNumber)) {
+    xCoordinates.add(buffer.int)
+  }
+  val yCoordinates = mutableListOf<Int>()
+  for (i in (0 until yCoordinatesNumber)) {
+    yCoordinates.add(buffer.int)
+  }
+
+  return NinePatchDescription(
+    leftPadding = leftPadding,
+    rightPadding = rightPadding,
+    topPadding = topPadding,
+    bottomPadding = bottomPadding,
+    xCoordinates = xCoordinates,
+    yCoordinates = yCoordinates,
+  )
 }
